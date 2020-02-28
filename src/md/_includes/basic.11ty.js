d53046e13ff673e9ea3ad71ef2aa1cedc5e4028d
kj <kristoferjoseph@gmail.com>
2020-02-28 13:15:54 -0800

    Updates universal header

--- src/md/_includes/basic.11ty.js
+++ src/md/_includes/basic.11ty.js
@@ -30,12 +30,23 @@ module.exports = function layout(params) {
 </head>
 <body style="opacity:0;">
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="0" height="0" style="display:none;">
-<symbol id="icon-question" viewBox="0 0 32 32"><path d="M7.955 4.375q1.75-0.739 3.682-0.739t3.682 0.739q1.75 0.75 3.017 2.017t2.017 3.017q0.739 1.75 0.739 3.682t-0.739 3.682q-0.75 1.75-2.017 3.017t-3.017 2.006q-1.75 0.75-3.682 0.75t-3.682-0.75q-1.75-0.739-3.017-2.006t-2.006-3.017q-0.75-1.75-0.75-3.682t0.75-3.682q0.739-1.75 2.006-3.017t3.017-2.017zM13.091 16q0-0.33 0.193-0.642t0.682-0.744q0.466-0.42 0.892-0.955t0.79-1.307q0.17-0.386 0.261-0.75t0.091-0.693q0-0.841-0.278-1.608t-0.801-1.392q-0.534-0.614-1.381-0.989t-1.903-0.375-1.903 0.375-1.369 0.989q-0.534 0.625-0.813 1.392t-0.278 1.608h2.909q0-0.057 0.011-0.153t0.091-0.358q0.068-0.25 0.205-0.438t0.443-0.347q0.148-0.080 0.324-0.119t0.381-0.040q0.295 0 0.534 0.080t0.386 0.193 0.261 0.278 0.159 0.301 0.074 0.29 0.040 0.21v0.102q0 0.42-0.307 0.915t-0.932 1.051q-0.341 0.307-0.58 0.563t-0.511 0.642q-0.284 0.398-0.432 0.864t-0.148 0.989v0.068h2.909zM10.614 19.932q0.42 0.432 1.023 0.432t1.034-0.432q0.42-0.42 0.42-1.023t-0.42-1.034q-0.432-0.42-1.034-0.42t-1.023 0.42q-0.432 0.432-0.432 1.034t0.432 1.023z"></path></symbol>
-<symbol id="icon-home" viewBox="0 0 32 32"><path d="M22.784 12q0.477 0.432 0.511 1.028t-0.364 1.085q-0.216 0.216-0.489 0.324t-0.602 0.108q-0.261 0-0.517-0.091t-0.506-0.273l-9.159-8.58-9.159 8.58q-0.443 0.432-1.045 0.403t-1-0.472q-0.42-0.443-0.392-1.045t0.472-1l9.159-8.58q0.818-0.761 1.966-0.761t1.966 0.761zM4.386 14.545l7.273-6.545 7.273 6.545v8q0 0.602-0.426 1.028t-1.028 0.426h-3.636v-5.818h-4.364v5.818h-3.636q-0.602 0-1.028-0.426t-0.426-1.028v-8z"></path></symbol>
-<symbol id="icon-dictionary" viewBox="0 0 32 32"><path d="M18.909 7.273q0.602 0 1.028 0.426t0.426 1.028v13.091q0 1.205-0.852 2.057t-2.057 0.852h-10.909q-1.205 0-2.057-0.852t-0.852-2.057v-16q0-1.795 1.284-3.080t3.080-1.284h10.909q0.602 0 1.028 0.426t0.426 1.028-0.426 1.028-1.028 0.426h-10.909q-0.602 0-1.028 0.426t-0.426 1.028 0.426 1.028 1.028 0.426h10.909z"></path></symbol>
+<symbol viewBox="0 0 16 14" id="icon-apps"><path fill-rule="evenodd" d="M12.26 8.215l1.207-.773L16 8.908 8 14 0 8.908l2.525-1.466 1.215.773-1.286.741L8 12.486l5.538-3.53-1.278-.74zM8 9.713L0 4.622 8 0l8 4.622-8 5.091zM2.454 4.677L8 8.207l5.538-3.53L8 1.474 2.454 4.677z"></path></symbol>
+<symbol id="icon-learn" viewBox="0 0 32 32"><path d="M18.909 7.273q0.602 0 1.028 0.426t0.426 1.028v13.091q0 1.205-0.852 2.057t-2.057 0.852h-10.909q-1.205 0-2.057-0.852t-0.852-2.057v-16q0-1.795 1.284-3.080t3.080-1.284h10.909q0.602 0 1.028 0.426t0.426 1.028-0.426 1.028-1.028 0.426h-10.909q-0.602 0-1.028 0.426t-0.426 1.028 0.426 1.028 1.028 0.426h10.909z"></path></symbol>
+
+<symbol viewBox="0 0 16 14" id="icon-docs"><path fill-rule="evenodd" d="M16 4.978l-3.338 6.163H10.16V14l-3.193-1.66L3.773 14v-2.86H2.54C.853 11.14 0 10.245 0 8.453 0 7.92.063 7.334.308 6.86 1.596 4.356 2.612 2.763 3.62 1.533 4.454.52 5.161 0 6.739 0h9.134l-4.127 6.874H2.54c-.436 0-1.089 0-1.089 1.563 0 1.519.626 1.519 1.089 1.519h1.233v-1.66h1.451v3.541l1.742-.904 1.741.904v-3.54h1.452v1.659h1.55l2.704-4.978H16zm-11.175-2.8c-.707.866-1.505 2.089-2.313 3.51h8.327l2.703-4.503H6.739c-.961 0-1.251.193-1.914.993z"></path></symbol>
+
+<symbol viewBox="0 0 16 14" id="icon-heart"><path d="M15.99 4.058c.07 1.062-.22 2.663-1.99 4.319-3.01 2.798-5.4 5.056-5.42 5.074L8 14l-.58-.549C7.4 13.433 5.01 11.175 2 8.377.23 6.72-.06 5.12.01 4.057c.08-1.24.7-2.401 1.66-3.103C2.5.35 3.59 0 4.68 0 6.33 0 7.38.639 8 1.413 8.62.639 9.67 0 11.32 0c1.09 0 2.18.35 3.01.954.96.702 1.58 1.862 1.66 3.104zm-3.14 3.32c1.37-1.278 1.6-2.465 1.54-3.24-.06-.989-.59-1.718-1.07-2.06-.54-.404-1.29-.638-2-.638-2.29 0-2.48 1.817-2.52 2.024-.05.234-.23.567-.77.567-.56 0-.79-.306-.83-.576-.03-.189-.23-2.015-2.52-2.015-.71 0-1.46.234-2 .638-.48.342-1.01 1.071-1.07 2.06-.06.775.17 1.962 1.54 3.24C5.28 9.366 7.11 11.076 8 11.913c.89-.837 2.72-2.547 4.85-4.535z" fill-rule="evenodd"></path></symbol>
+
 <symbol viewBox="0 0 18 12" id="icon-menu"><g fill-rule="evenodd"><rect width="18" height="2" rx="1"></rect><rect y="5" width="18" height="2" rx="1"></rect><rect y="10" width="18" height="2" rx="1"></rect></g></symbol>
+
 <symbol viewBox="0 0 16 10" id="icon-disclose"><path d="M5.762 8L.475 2.725C.158 2.4 0 2.025 0 1.6 0 1.175.158.804.475.487.8.162 1.175 0 1.6 0c.425 0 .8.158 1.125.475L8 5.762 13.275.475C13.6.158 13.975 0 14.4 0c.425 0 .796.162 1.112.487.326.317.488.688.488 1.113 0 .425-.158.8-.475 1.125L10.238 8l-1.44 1.44c-.262.272-.528.407-.798.405-.27-.002-.54-.141-.81-.417L5.762 8z" fill-rule="evenodd"></path></symbol>
+
 <symbol viewBox="0 0 11 11" id="icon-left-arrow"><path d="M5.385 0l5.518 5.517-5.518 5.518-.751-.745 4.245-4.245H0V4.99h8.88L4.633.745z" fill-rule="nonzero"></path></symbol>
+
+<symbol viewBox="0 0 16 14" id="icon-support"><path fill-rule="evenodd" d="M12.462 0C14.413 0 16 1.504 16 3.354v3.792c0 1.85-1.587 3.354-3.538 3.354H9.317l-3.702 3.3a.852.852 0 0 1-.538.2.821.821 0 0 1-.298-.055.735.735 0 0 1-.471-.674V10.5h-.77C1.587 10.5 0 8.996 0 7.146V3.354C0 1.504 1.587 0 3.538 0h8.924zm2 7.146V3.354c0-1.048-.895-1.896-2-1.896H3.538c-1.105 0-2 .848-2 1.896v3.792c0 1.048.895 1.896 2 1.896h2.308v2.606l2.856-2.606h3.76c1.105 0 2-.848 2-1.896z"></path></symbol>
+
+<symbol viewBox="0 0 15 16" id="icon-settings"><path d="M14.636 9.852a1.1 1.1 0 0 1 .233 1.335l-.933 1.626c-.187.32-.552.53-.934.53a.986.986 0 0 1-.342-.062l-1.377-.484c-.466.375-.98.672-1.54.89l-.264 1.446A1.096 1.096 0 0 1 8.436 16H6.57c-.497 0-.956-.383-1.042-.867l-.272-1.445a5.958 5.958 0 0 1-1.54-.891l-1.377.484a1.092 1.092 0 0 1-1.268-.469l-.934-1.624a1.087 1.087 0 0 1 .226-1.336l1.105-.954A5.883 5.883 0 0 1 1.404 8c0-.297.016-.594.063-.898L.362 6.148a1.087 1.087 0 0 1-.226-1.335l.934-1.625c.241-.415.809-.633 1.268-.47l1.377.485a5.958 5.958 0 0 1 1.54-.89L5.527.867A1.083 1.083 0 0 1 6.57 0h1.867c.49 0 .95.383 1.043.867l.264 1.446c.56.218 1.074.515 1.54.89l1.377-.484c.46-.164 1.027.047 1.276.469l.933 1.624a1.1 1.1 0 0 1-.233 1.336l-1.105.954a5.903 5.903 0 0 1 0 1.796l1.105.954zm-.63.835c.008-.015 0-.07-.016-.078l-1.54-1.328.055-.289c.062-.328.1-.664.1-.992 0-.328-.038-.664-.1-.992l-.055-.29 1.54-1.327c.016-.008.024-.063.016-.079l-.934-1.625a.146.146 0 0 0-.07-.03l-1.929.679-.217-.188a4.968 4.968 0 0 0-1.712-.992l-.272-.094-.373-2.007C8.49 1.03 8.452 1 8.436 1H6.57c-.023 0-.062.031-.062.055l-.38 2.008-.273.093a4.967 4.967 0 0 0-1.711.992l-.218.188-1.914-.68a.191.191 0 0 0-.078.031L1 5.313c-.008.015 0 .07.015.078l1.533 1.328-.055.289a5.355 5.355 0 0 0 0 1.984l.055.29-1.54 1.327c-.008.008-.016.063-.008.079l.933 1.624a.117.117 0 0 0 .07.032l1.922-.68.218.188a4.968 4.968 0 0 0 1.71.992l.273.094.381 2.007c0 .024.04.055.062.055h1.867c.016 0 .055-.031.063-.055l.373-2.008.272-.093a4.968 4.968 0 0 0 1.712-.992l.217-.188 1.922.68c.046 0 .07-.024.077-.031l.934-1.626zM7.503 4.75c1.781 0 3.236 1.46 3.236 3.25s-1.455 3.25-3.236 3.25c-1.781 0-3.236-1.46-3.236-3.25s1.455-3.25 3.236-3.25zm0 6A2.748 2.748 0 0 0 10.24 8a2.748 2.748 0 0 0-2.738-2.75A2.748 2.748 0 0 0 4.765 8a2.748 2.748 0 0 0 2.738 2.75z" fill-rule="evenodd"></path></symbol>
+
 </svg>
 <div class="vh-100 d-flex fd-c o-hidden">
 <header
@@ -44,13 +55,9 @@ module.exports = function layout(params) {
     d-flex
     ai-c
     jc-b
-    pt-1
-    pr1
-    pb-1
-    pl1
+    p0
     bg-p25
   "
-  style="min-height: 3.333rem;"
 >
   <div
     class="
@@ -60,25 +67,46 @@ module.exports = function layout(params) {
     "
   >
     <div
-      class="d-none-lg mr-1"
-      style="margin-top:-0.111rem;"
+      class="
+        d-none-lg
+        mr-1
+      "
+      style="
+        margin-top:-0.111rem;
+      "
     >
       <a
-        id=js-menu
-        class=f-p1 bg-p0
-        href=/menu
+        class="
+          f-p1
+          bg-p0
+        "
+        href="#"
       >
-        <div style="width:1rem;height:0.666rem;" class="">
-        <svg>
-          <use xlink:href="#icon-menu"></use>
-        </svg>
+        <div
+          style="
+            width:1rem;
+            height:0.666rem;
+          "
+        >
+          <svg>
+            <use xlink:href="#menu"></use>
+          </svg>
         </div>
       </a>
     </div>
-    <a class="mr4" href="https://begin.com/">
-      <div class="logo d-flex ai-c">
+    <a
+      class="mr4"
+      href="/apps"
+    >
+      <div
+        class="
+          logo
+          d-flex
+          ai-c
+        "
+      >
         <img
-          src="/assets/begin-logo.svg"
+          src="https://static.begin.com/web/begin-logo.svg"
           alt="begin logo"
           style="min-width:4.777rem;min-height:1.333rem;"
           width="86"
@@ -87,44 +115,43 @@ module.exports = function layout(params) {
       </div>
     </a>
     <nav
-      id="js-nav"
       class="
         w-100
-        max-h-infinity-lg
-        max-h-0
+        mh-none
+        mh-infinity-lg
         p-absolute
         p-static-lg
         d-flex-lg
         jc-b
         pr-4
         pl-4
-        bg-p25
         o-hidden
+        bg-p25
         menu-transition
         z1
         z0-lg
       "
       style="
-        top:2.8rem;
+        top:3rem;
         right:0;
         left:0;
         border-bottom-right-radius:6px;
         border-bottom-left-radius:6px;
       "
-      >
-      <div
+    >
+      <span
         class="
           d-flex-lg
         "
       >
         <a
-          href=/
+          alt="/apps"
           class="
             d-flex
             ai-c
             fs-off-scale
             fw-medium
-            upper
+            uppercase
             lh2
             pr0
             pl-1
@@ -137,30 +164,35 @@ module.exports = function layout(params) {
             mb-2
             mb-none-lg
             mr1-lg
-            cursor-pointer
-            active
           "
+          href="https://begin.com/apps"
+          target="_blank"
+          rel="noopener"
         >
           <div
-            class="mr-2 f-p26"
-            style="width:1.333rem;height:1.333rem;"
+            class="
+              mr-2
+              f-p26
+            "
+            style="
+              width:0.888rem;
+              height:0.777rem;
+            "
           >
             <svg>
-              <use xlink:href="#icon-home"></use>
+              <use xlink:href="#icon-apps"></use>
             </svg>
           </div>
-          <span>
-            Home
-          </span>
+          <span>Apps</span>
         </a>
         <a
-          href=/faq
+          alt="https://docs.begin.com"
           class="
             d-flex
             ai-c
             fs-off-scale
             fw-medium
-            upper
+            uppercase
             lh2
             pr0
             pl-1
@@ -172,84 +204,236 @@ module.exports = function layout(params) {
             transition-all
             mb-2
             mb-none-lg
-            mr1-l
-            cursor-pointer
+            mr1-lg
           "
+          href="https://docs.begin.com"
+          target="_blank"
+          rel="noopener"
         >
-          <div
-            class="mr-2 f-p26"
-            style="width:1.333rem;height:1.333rem;"
-          >
-            <svg>
-              <use xlink:href="#icon-question"></use>
-            </svg>
-          </div>
-          <span>
-            FAQ
-          </span>
-        </a>
-        <a
-          href=/jargon
+        <div
           class="
-            d-flex
-            ai-c
-            fs-off-scale
-            fw-medium
-            upper
-            lh2
-            pr0
-            pl-1
-            c-p26
-            c-h3
-            c-a5
-            bg-a7
-            br-pill
-            transition-all
-            mb-2
-            mb-none-lg
-            mr1-l
-            cursor-pointer
-
+            mr-2
+            f-p26
+          "
+          style="
+            width:0.888rem;
+            height:0.777rem;
           "
         >
-          <div
-            class="mr-2 f-p26"
-            style="width:1.25rem;height:1.25rem;"
+          <svg>
+            <use xlink:href="#icon-docs"></use>
+          </svg>
+        </div>
+        <span>Docs</span>
+       </a>
+       <a
+        alt="https://spectrum.chat/begin"
+        class="
+          d-flex
+          ai-c
+          fs-off-scale
+          fw-medium
+          uppercase
+          lh2
+          pr0
+          pl-1
+          c-p26
+          c-h3
+          c-a5
+          bg-a7
+          br-pill
+          transition-all
+          mb-2
+          mb-none-lg
+          mr1-lg
+        "
+        href="https://spectrum.chat/begin"
+        target="_blank"
+        rel="noopener"
+      >
+        <div
+          class="
+            mr-2
+            f-p26
+          "
+          style="
+            width:0.888rem;
+            height:0.777rem;
+          "
+        >
+          <svg>
+            <use xlink:href="#icon-heart"></use>
+          </svg>
+        </div>
+        <span>Community</span>
+      </a>
+      <a
+        alt="https://begin-help.zendesk.com"
+        class="
+          d-flex
+          ai-c
+          fs-off-scale
+          fw-medium
+          uppercase
+          lh2
+          pr0
+          pl-1
+          c-p26
+          c-h3
+          c-a5
+          bg-a7
+          br-pill
+          transition-all
+          mb-2
+          mb-none-lg
+          mr1-lg
+        "
+        href="https://begin-help.zendesk.com"
+        target="_blank"
+        rel="noopener"
+      >
+        <div
+          class="
+            mr-2
+            f-p26
+          "
+          style="
+            width:0.888rem;
+            height:0.777rem;
+          "
+        >
+          <svg>
+            <use xlink:href="#icon-support"></use>
+          </svg>
+        </div>
+        <span>Support</span>
+      </a>
+      </span>
+        <span
+          class="
+            mb0
+            mb-none-lg
+            d-flex
+            fd-c
+            fd-r-lg
+          "
+        >
+          <a
+            alt="/account"
+            class="
+              d-flex
+              ai-c
+              fs-off-scale
+              fw-medium
+              uppercase
+              lh2
+              pr0
+              pl-1
+              c-p26
+              c-h3
+              c-a5
+              bg-a7
+              br-pill
+              transition-all
+              mb-2
+              mb-none-lg
+            "
+            href="/account"
+            target="_self"
           >
-            <svg>
-              <use xlink:href="#icon-dictionary"></use>
-            </svg>
-          </div>
-          <span>
-            Jargon
-          </span>
-        </a>
-      </div>
-    </nav>
-  </div>
-  <div>
-    <a
-      id="js-disclose"
-      href="#"
-      class="
-        d-block
-        d-none-lg
-        p-4
-        bg-p0
-        transition-rotate
+          <div
+            class="
+              mr-2
+              f-p26
+            "
+            style="
+              width:0.888rem;
+              height:0.777rem;
+            "
+          >
+          <svg>
+            <use xlink:href="#icon-settings"></use>
+          </svg>
+        </div>
+        <span class="d-none-lg">Account settings</span>
+      </a>
+      <a
+        id="js-account-link"
+        href="#"
+        class="
+          d-none
+          d-flex
+          ai-c
+          fs-off-scale
+          fw-medium
+          uppercase
+          lh2
+          pr0
+          pl-3
+          pr-none-lg
+          pl-none-lg
+          c-p26
+          c-h3
+          c-a5
+          bg-a7
+          br-pill
+          transition-all
+          mb-2
+          mb-none-lg
+        "
+      >
+        <div
+          class="
+            avatar
+            mr-3
+            mr-none-lg
+          "
+        >
+          <img
+            id="js-avatar"
+            alt="Profile avatar"
+            class="
+              d-none
+              br-100
+              o-hidden
+            "
+            style="
+              object-fit: cover;
+            "
+          >
+        </div>
+        <span class="d-none-lg">Your profile</span>
+      </a>
+    </span>
+  </nav>
+</div>
+<div id="disclose-container">
+  <a
+    id="js-disclose"
+    href="#"
+    class="
+      d-block
+      d-none-lg
+      p-4
+      bg-p0
+      transition-rotate
+    "
+  >
+    <div
+      class="f-p26"
+      style="
+        width:0.888rem;
+        height:0.555rem;
       "
     >
-      <div
-        class="f-p26"
-        style="width:0.888rem;height:0.555rem;"
-      >
-        <svg>
-          <use xlink:href="#icon-disclose"></use>
-        </svg>
-      </div>
-    </a>
-  </div>
+      <svg>
+        <use xlink:href="#icon-disclose"></use>
+      </svg>
+    </div>
+  </a>
+</div>
 </header>
+
 <div class="h-100 top-0 p-relative p-static-lg fg-1 d-flex-lg o-hidden">
 <div
   class="
@@ -279,10 +463,7 @@ module.exports = function layout(params) {
           fs-1
           fw-medium
           c-p8
-          uppercase
-          nowrap
-          cu-pointer
-        "
+          uppercase nowrap cu-pointer "
       >
         Foundations
       </h2>
@@ -673,7 +854,7 @@ module.exports = function layout(params) {
           uppercase
           nowrap
           cu-pointer
-        "
+          uppercase
       >
         Backend patterns
       </h2>
